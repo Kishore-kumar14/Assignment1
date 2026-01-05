8a1611659e285a4018aff0c996ffc4a4a51f1d64
vigneshwaran21-vicky <vigneshwaran.2023b@vitstudent.ac.in>
2026-01-05 22:57:14 +0530

    Added password validation program

---
+++ password_validation.py
@@ -0,0 +1,16 @@
+import re
+
+passwords = input("Enter passwords: ").split(',')
+
+valid_passwords = []
+
+for pwd in passwords:
+    pwd = pwd.strip()
+    if (6 <= len(pwd) <= 12 and
+        re.search("[a-z]", pwd) and
+        re.search("[A-Z]", pwd) and
+        re.search("[0-9]", pwd) and
+        re.search("[$#@]", pwd)):
+        valid_passwords.append(pwd)
+
+print(",".join(valid_passwords))
